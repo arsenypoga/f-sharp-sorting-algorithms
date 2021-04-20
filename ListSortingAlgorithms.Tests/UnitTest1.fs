eae7f30aaf8a8d4c1d4161bc69e8dce81c7e60f1
Travis Fletcher <travis@tfletch.tech>
2021-04-19 23:17:23 -0400

    Added mergeSort and quickSort

--- ListSortingAlgorithms.Tests/UnitTest1.fs
+++ ListSortingAlgorithms.Tests/UnitTest1.fs
@@ -26,27 +26,42 @@ let dataset () =
         args = [ 5; 7; 3; 2; 1; 8; 9; 0; 14; 17 ]
         expected = [ 0; 1; 2; 3; 5; 7; 8; 9; 14; 17 ] } ]
 
+let assertDataSetIsValidForRow row = 
+  match row.name with 
+  | "Empty List" -> ()
+  | "List of 1 element" -> ()
+  | "Sorted List" -> ()
+  | _ -> row.args |> should not' (equal row.expected)
 
 
 [<Test>]
 [<TestCaseSource("dataset")>]
+let TestQuickSort (row) =
+    assertDataSetIsValidForRow row
+
+    quickSort row.args
+    |> should equal row.expected
+
+[<Test>]
+[<TestCaseSource("dataset")>]
+let TestMergeSort (row) =
+    assertDataSetIsValidForRow row
+
+    mergeSort row.args
+    |> should equal row.expected
+
+[<Test>]
+[<TestCaseSource("dataset")>]
 let TestSelectionSort (row) =
-    match row.name with
-    | "Empty List" -> ()
-    | "List of 1 element" -> ()
-    | "Sorted List" -> ()
-    | _ -> row.args |> should not' (equal row.expected)
+    assertDataSetIsValidForRow row
 
     selectionSort row.args
     |> should equal row.expected
 
 [<Test>]
 [<TestCaseSource("dataset")>]
+[ignore("Not Implemented")]
 let TestBubbleSort (row) =
-    match row.name with
-    | "Empty List" -> ()
-    | "List of 1 element" -> ()
-    | "Sorted List" -> ()
-    | _ -> row.args |> should not' (equal row.expected)
+    assertDataSetIsValidForRow row
 
     bubbleSort row.args |> should equal row.expected
--- ListSortingAlgorithms/Library.fs
+++ ListSortingAlgorithms/Library.fs
@@ -25,6 +25,43 @@ let rec selectionSort =
 
         smallest :: selectionSort rest
 
+// Given a tuple of 2 sorted lists ([], []), combine them to form a single sorted list ([]).
+let rec private _mergeParts = function
+    // If either list is empty, return the other
+    | ([], list) | (list, []) -> list
+    // If the head of list1 is greater than the head of list2, pop it off and keep sorting recursively
+    | (head :: rest, other) when head <= other.Head -> [head] @ (_mergeParts (rest, other))
+    // If the head of list2 is greater than the head of list1, pop it off and keep sorting recursively
+    | (other, head :: rest) when head <= other.Head -> [head] @ (_mergeParts (rest, other))
+    
+    // If we get passed something unexpected, just return nothing (I guess we could throw up...)
+    | _ -> []
+
+// Given a list, sort it.
+let rec mergeSort = function
+    // If the list has less than 2 elements, just return it as-is (base case)
+    | [] | [_] as list -> list
+    
+    // If there are only two elements left, return them sorted
+    | [first; last] when first <= last -> [first; last]
+    | [first; last] when first >= last -> [last; first]
+
+    // Otherwise, recursively mergesort by splitting the array in two and sorting each half
+    // and then combine the results using _mergeParts
+    | list -> list |> List.splitAt (list.Length / 2) |> fun (head, tail) -> _mergeParts((mergeSort head), (mergeSort tail))
+
+let rec quickSort = function
+    // If the list has less than 2 elements, just return it as-is (base case)
+    | [] | [_] as list -> list
+    // Otherwise, use the head as a pivot and return sorted values under the pivot,
+    // the pivot, and then the sorted values over the pivot.
+    | pivot :: list ->
+        // This filters down to values smaller than the pivot and then recursively sorts them
+        (quickSort (List.filter ((>=) pivot) list)) @ 
+        [pivot] @
+        // This filters down to values larger than the pivot and then recursively sorts them
+        (quickSort (List.filter ((<=) pivot) list))
+
 let rec bubbleSort =
     function
     | [] -> []
